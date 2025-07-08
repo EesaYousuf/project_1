83d4036feff3b575e8b2eb7c4d51d919edb9fd9e
Eesa yousuf <eesayousufyousuf@gmail.com>
2025-07-08 16:06:03 +0530

    itr2

--- ITERATOR.py
+++ ITERATOR.py
@@ -2,3 +2,11 @@ class SquareIterator:
     def __init__(self, max_number):
         self.n = 0
         self.max = max_number
+ def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.n <= self.max:
+            result = self.n ** 2
+            self.n += 1
+            return result
